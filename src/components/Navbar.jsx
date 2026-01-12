31d672a3b77f5b3528b8b837f7d8041fc15e72b6
Pushkar Singh <singhpushkarpss@gmail.com>
2026-01-12 23:26:25 +0530

    add to cart or wishlist symbol showed on navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import { FiUser, FiHeart, FiShoppingBag, FiMenu, FiX,FiSearch} from "react-icons/fi";
-import { RiQrCodeLine} from "react-icons/ri";
-
+import {
+  FiUser,
+  FiHeart,
+  FiShoppingBag,
+  FiMenu,
+  FiX,
+  FiSearch,
+} from "react-icons/fi";
+import { RiQrCodeLine } from "react-icons/ri";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,28 +18,28 @@ export default function Navbar() {
           <div className="flex gap-20 justify-between h-16 items-center">
             <div className="flex">
               {/* Mobile Menu Button */}
-            <div className="md:hidden flex items-center mr-3">
-              <button onClick={() => setIsOpen(!isOpen)}>
-                {isOpen ? (
-                  <FiX className="text-2xl" />
-                ) : (
-                  <FiMenu className="text-2xl" />
-                )}
-              </button>
-            </div>
-            {/* Logo */}
-            <div className="flex-shrink-0">
-              <img
-                src="/logo-navbar.png"
-                alt="Shifa Logo"
-                className="h-12 w-auto"
-              />
-            </div>
+              <div className="md:hidden flex items-center mr-3">
+                <button onClick={() => setIsOpen(!isOpen)}>
+                  {isOpen ? (
+                    <FiX className="text-2xl" />
+                  ) : (
+                    <FiMenu className="text-2xl" />
+                  )}
+                </button>
+              </div>
+              {/* Logo */}
+              <div className="flex-shrink-0">
+                <img
+                  src="/logo-navbar.png"
+                  alt="Shifa Logo"
+                  className="h-12 w-auto"
+                />
+              </div>
             </div>
 
             {/* Right Icons */}
             <div className="md:flex justify-between w-[68vw] lg:w-[75vw] xl:w-[70vw]">
-              <div className="flex items-center justify-between w-[50vw] md:w-[78vw]">
+              <div className="flex items-center justify-between w-[50vw] md:w-[65vw] lg:w-[74vw]">
                 <h1 className="hidden lg:block font-semibold text-lg hover:text-pink-400">
                   Home
                 </h1>
@@ -52,23 +58,27 @@ export default function Navbar() {
 
                 <div className="flex justify-between items-center w-[15vw] md:w-[35vw] lg:w-[27vw] xl:w-[21vw]">
                   <div className="hidden md:flex items-center gap-1.5">
-                  <FiUser className=" text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
-                  <h1 className=" hidden md:block text-lg font-medium  hover:text-pink-600">
-                    Account
-                  </h1>
-                </div>
-                <div className="flex items-center gap-1.5">
-                  <FiHeart className="text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
-                  <h1 className="hidden md:block text-lg font-medium  hover:text-pink-600">
-                    Wishlist
-                  </h1>
-                </div>
-                <div className="flex items-center gap-1.5">
-                  <FiShoppingBag className="text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
-                  <h1 className="hidden md:block text-lg font-medium  hover:text-pink-600">
-                    Cart
-                  </h1>
-                </div>
+                    <FiUser className=" text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
+                    <h1 className=" hidden md:block text-lg font-medium  hover:text-pink-600">
+                      Account
+                    </h1>
+                  </div>
+                  <div className="flex items-center gap-1.5 ">
+                    <div className="relative">
+                      <FiHeart className="text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
+                    <div className="absolute bottom-4 right-[-0.6vw] bg-pink-400 font-semibold text-xs text-white text-center rounded-4xl md:bottom-3 md:right-[-0.4vw] xl:right-[-0.1vw] p-1"></div>
+                    </div>
+                    <h1 className="hidden md:block text-lg font-medium  hover:text-pink-600">
+                      Wishlist
+                    </h1>
+                    </div>
+                  <div className="flex items-center gap-1.5 relative">
+                    <FiShoppingBag className="text-2xl md:text-xl cursor-pointer hover:text-pink-600" />
+                    <h1 className="hidden md:block text-lg font-medium  hover:text-pink-600">
+                      Cart
+                    </h1>
+                    <p className="absolute bottom-4 right-[-2vw] bg-pink-400 w-[4vw] font-semibold text-xs text-white text-center rounded-4xl md:text-xs md:w-[2.2vw] md:right-[-1.9vw] lg:w-[1.8vw] lg:right-[-1.4vw] xl:w-[1.2vw] xl:right-[-1vw]">1</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -87,7 +97,6 @@ export default function Navbar() {
             </ul>
           </div>
         )}
-      
       </nav>
 
       {/* <div className="mt-18 flex items-center justify-center gap-2 md:hidden">
